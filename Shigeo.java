b8b6e3ee72aec2e797566dc1e74f4aada124ab32
Talles Souza <tallessouza117@gmail.com>
2025-06-22 16:27:32 -0300

    Adicionando a comemoração de vitória

--- Shigeo.java
+++ Shigeo.java
@@ -32,6 +32,18 @@ public class Shigeo extends AdvancedRobot {
     public void onHitRobot(HitRobotEvent e) {
     }
 
+    // == Comemoração de vitória == //
+
     public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; i++) {
+            // Troca cor do corpo
+            setBodyColor(Color.getHSBColor((float)Math.random(), 1.0f, 1.0f));
+            setGunColor(Color.getHSBColor((float)Math.random(), 1.0f, 1.0f));
+            setRadarColor(Color.getHSBColor((float)Math.random(), 1.0f, 1.0f));
+            // "Dança" girando
+            turnRight(25);
+            turnLeft(25);
+            execute();
+        }
     }
 }
